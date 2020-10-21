ed07cf3210e8fdf0656b8624c22b00e478b67d3e
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-21 11:28:48 -0300

    test: create test to ensure PollController.store validates data

--- tests/Feature/PollControllerTest.php
+++ tests/Feature/PollControllerTest.php
@@ -23,10 +23,50 @@ class PollControllerTest extends TestCase
             ]
         ];
 
-        $response = $this->postJson('/api/poll', $data)->assertStatus(201);
+        $response = $this->postJson('/api/poll', $data);
+        $response->dump();
+        $response->assertStatus(201);
+
         $this->assertDatabaseHas('polls', [ 'id' => $response['poll_id']]);
         $this->assertDatabaseCount('options', 3);
     }
 
+    /** @test */
+    public function shouldReturn422IfPollDescriptionIsInvalid()
+    {
+        $dataWithoutPollDescription = [
+            'options' => [
+                'options 1',
+                'options 2',
+                'options 3'
+            ]
+        ];
 
+        $this->postJson('/api/poll', $dataWithoutPollDescription)
+            ->assertStatus(422);
+
+        $dataWithInvalidPollDescription = [
+            'poll_description' => 123,
+            'options' => [
+                'options 1',
+                'options 2',
+                'options 3'
+            ]
+        ];
+
+        $response = $this->postJson('/api/poll', $dataWithInvalidPollDescription)
+            ->assertStatus(422);
+    }
+
+    /** @test */
+    public function shouldReturn422IfOptionsAreInvalid()
+    {
+        $dataWithInvalidOptions= [
+            'poll_description' => 'test description',
+            'options' => []
+        ];
+
+        $this->postJson('/api/poll', $dataWithInvalidOptions)
+            ->assertStatus(422);
+    }
 }
